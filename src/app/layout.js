e2264c5258067a484fcac0b786abcfb30e34efb5
MrBigboka <57997492+MrBigboka@users.noreply.github.com>
2024-08-18 15:31:27 -0400

    feat: avatar + image

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}
-<script src="https://cdn.botpress.cloud/webchat/v2.1/inject.js"></script>
-<script src="https://mediafiles.botpress.cloud/737e0698-814f-44b4-b08e-e2240bfbeeb5/webchat/v2.1/config.js"></script>
+        <script src="https://cdn.botpress.cloud/webchat/v2.1/inject.js"></script>
+        <script src="https://mediafiles.botpress.cloud/737e0698-814f-44b4-b08e-e2240bfbeeb5/webchat/v2.1/config.js"></script>
       </body>    
     </html>
   );
