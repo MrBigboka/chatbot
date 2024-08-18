98d61593ce41c2a3a3dd2c7359be99db84dac9dd
MrBigboka <57997492+MrBigboka@users.noreply.github.com>
2024-08-18 10:09:14 -0400

    feat: better visuals

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h4 className={styles.status}>BETA</h4>
       <h1 className={styles.gradientText}>DEMO SOFIA</h1>
       <div className={styles.poweredBy}>
         <span>Propulsé par</span>
         <Image
           src="/logo.png" // Chemin vers votre logo dans le dossier public
           alt="Logo"
-          width={100}
+          width={150}
           height={20}
           className={styles.logo}
         />
